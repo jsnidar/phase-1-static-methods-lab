4a5218c47eff6c5e84b9e80c9e6170f13ec035bc
Jeremiah <jsnidar@gmail.com>
2021-08-31 15:13:50 -0500

    commit tree 501028dd38449c221e8c622894e1c5123a3c8a39

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  static capitalize (string) {
+    return string.substring(0,1).toUpperCase() + string.slice(1)
+  }
+  static sanitize (string) {
+    return string.replace(/[^-,'A-Za-z0-9 ]+/g, '');
+  }
+  
+  static titleize (string) {
+    let stringArr = string.split(" ")
+    let newString = []
+    stringArr.forEach(word => {
+      if(
+        (word !== 'the') && 
+        (word !== 'a') && 
+        (word !== 'and') && 
+        (word !== 'of') && 
+        (word !== 'an') && 
+        (word !== 'but') && 
+        (word !== 'for') && 
+        (word !== 'at') && 
+        (word !== 'by') && 
+        (word !== 'from')) {
+        return newString.push(word.substr(0,1).toUpperCase() + word.slice(1))
+      }else{
+        return newString.push(word)
+      }
+    })
+    string = newString.join(' ')
+    return string.substr(0,1).toUpperCase() + string.slice(1)
+  }
 }
